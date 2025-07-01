61a24b13cdd18b3bde3a21eff2ccafd84725d64e
Carlos Eduardo Salles de Sousa <carloseduardo.rss@gmail.com>
2025-07-01 05:24:21 -0300

    Fix: Remove namespaces não utilizados de algumas classes

--- jogos/Jogo/Assets/Scripts/Quiz/Alternativa.cs
+++ jogos/Jogo/Assets/Scripts/Quiz/Alternativa.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
-using UnityEditor.Compilation;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
